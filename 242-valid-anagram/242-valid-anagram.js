01e8533b85bdb76d2b8f059dbfae385fb5b35b2a
JRusinova <49450930+jenrusinova@users.noreply.github.com>
2022-06-01 13:59:48 -0700

    Time: 102 ms (63.10%), Space: 46.2 MB (32.71%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.js
@@ -0,0 +1,37 @@
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {boolean}
+ */
+var isAnagram = function(s, t) {
+
+ let arr1 =  s.split('');
+let arr2 = t.split('');
+  
+  let sort  = function (a, b){
+    if (a <= b){
+       return -1;
+     } else {
+       return 1;
+     }
+  }
+    
+  arr1.sort();
+  arr2.sort();
+  console.log(arr1);
+console.log(arr2);
+     
+  if(arr1.length !== arr2.length) {
+return false; 
+  }
+     for(let i =0; i < arr1.length; i++){
+       if (arr1[i] !== arr2[i]){
+          return false;
+       }
+          
+     }
+     
+  return true;
+
+    
+};
