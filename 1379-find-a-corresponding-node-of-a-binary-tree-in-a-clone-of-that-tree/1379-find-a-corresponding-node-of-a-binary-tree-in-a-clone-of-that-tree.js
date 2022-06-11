0cc3fa343a018f27bcbf27146681392222ee3ae3
JRusinova <49450930+jenrusinova@users.noreply.github.com>
2022-06-11 16:37:09 -0700

    Time: 342 ms (63.70%), Space: 87.1 MB (47.47%) - LeetHub

---
+++ 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.js
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} original
+ * @param {TreeNode} cloned
+ * @param {TreeNode} target
+ * @return {TreeNode}
+ */
+
+var getTargetCopy = function(original, cloned, target) {
+  
+  if (cloned === null){
+    return null;
+  }
+  
+  let stack = [];
+  stack.push(cloned);
+  
+  while(stack.length){
+    let current = stack.pop();
+
+      if (current!= null) {
+         if(current.val == target.val){
+            return current;
+         }  
+        stack.push(current.left);
+        stack.push(current.right);
+         
+      } 
+    }
+  }
+  
+  
