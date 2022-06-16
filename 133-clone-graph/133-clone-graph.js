2caf94ce568915a6f2f6d8cfd9df08e7efc31d07
JRusinova <49450930+jenrusinova@users.noreply.github.com>
2022-06-15 20:25:01 -0700

    Time: 93 ms (41.38%), Space: 44.2 MB (21.14%) - LeetHub

--- 133-clone-graph/133-clone-graph.js
+++ 133-clone-graph/133-clone-graph.js
@@ -17,43 +17,46 @@ var cloneGraph = function(node) {
     return null;
   }
   
+ 
   //create new hashmap
-  let visited = new Map();
-  //initialize it with the first node
-  visited.set(node, new Node (node.val))  
+  let cloned = new Map();
   
+    
+  //initialize it with the first node
+  cloned.set(node, new Node (node.val))  
+  
+     
   //initialize queue with the first node
   let queue = [];
   queue.push(node);
-  
 
   while (queue.length){
     
-    let current = queue.shift();
+    let original = queue.shift();
     
     //checking all the iterable neighbors of the current node
-    for (let neighbor of current.neighbors) {
+    for (let neighbor of original.neighbors) {
       
  //if we didn't put the neighbor in the map
       
-      if (!visited.has(neighbor)){
+      if (!cloned.has(neighbor)){
         
         //put it there copying the value
-         visited.set(neighbor, new Node (neighbor.val));
+        
+         cloned.set(neighbor, new Node (neighbor.val));
+        
         //add neighbor to the queue to continue bfs
          queue.push (neighbor);
       }
-      
-      
-      //receive current node neigbors array
-      let currentNeigh = visited.get(current).neighbors;
-      
-     
+           
+      //receive copy of current node neighbors
+      let currentNeigh = cloned.get(original).neighbors;
+           
       //push there cloned neighbor
-      currentNeigh.push(visited.get(neighbor));
+      currentNeigh.push(cloned.get(neighbor));
     }
   }
   
-  return visited.get(node);
+  return cloned.get(node);
   
 };
