1b54ee5e28ef38d4fe2ef1375a78ae8088770390
JRusinova <49450930+jenrusinova@users.noreply.github.com>
2022-07-13 18:41:17 -0700

    Time: 80 ms (57.64%), Space: 42.1 MB (16.24%) - LeetHub

---
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var tribonacci = function(n) {
+    let arr = [] ;
+  
+    arr[0] = 0;
+  arr[1] = 1;
+  arr[2] = 1;
+  
+  for (let i = 3; i <=n; i++){
+     arr[i] = arr[i - 1] + arr[ i - 2] + arr[ i - 3];
+  }
+  
+  return arr[n];
+};
