9a802238ac9a5ade3cb12943ef5bdf5fd57b8abd
JRusinova <49450930+jenrusinova@users.noreply.github.com>
2022-07-13 18:06:32 -0700

    Time: 61 ms (93.33%), Space: 41.9 MB (56.60%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.js
+++ 70-climbing-stairs/70-climbing-stairs.js
@@ -3,21 +3,17 @@
  * @return {number}
  */
 var climbStairs = function(n) {
-    if (n === 1) {
-           return 1;
-        }
-    if (n === 2) {
-        return 2;
+    if (n === 1){
+      return 1;
     }
   
-  let current = 0;
+    let dp = [];
   
-    let one = 2;
-    let two = 1;
-    for (let i = 2; i < n; i++) {
-      current = one + two;
-      two = one;
-      one = current;
-    }
-    return current;
+  dp[1] = 1;
+  dp[2] = 2;
+  
+  for (let i =3; i < n + 1; i++){
+     dp[i] = dp[i - 1] + dp[i - 2];
+  }
+  return dp[n];
 };
