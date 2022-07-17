bd0567fb2507661beb078d62831e821379515f3e
JRusinova <49450930+jenrusinova@users.noreply.github.com>
2022-07-16 17:41:33 -0700

    Time: 96 ms (80.16%), Space: 45.6 MB (55.35%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.js
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.js
@@ -9,18 +9,16 @@ var topKFrequent = function(nums, k) {
     return [];
   }
   let obj = {};
+  //O(n)
   for (let i =0; i < nums.length; i++){
     if (obj[nums[i]] === undefined){
       obj[nums[i]] = 1;
     } else obj[nums[i]]++;
   }
-  //obj = {1: 3, 2: 2, 3:1 }
-  console.log('obj', obj);
-  //check values in obj, if they are in  max + 1 - k range
-  //push keys in arr and return it
-  
+  //(O (nlog n))
   let sortedKeys = Object.keys(obj).sort((a,b)=>obj[b]-obj[a]);
    console.log(sortedKeys);
   
   return sortedKeys.slice(0, k);
 };
+// Time complexity is O(nlogn)
