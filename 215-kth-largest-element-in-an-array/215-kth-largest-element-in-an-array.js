9559c9085ce6d468c0d87c430c4600c2b770ff2e
JRusinova <49450930+jenrusinova@users.noreply.github.com>
2022-07-16 17:45:41 -0700

    Time: 209 ms (8.95%), Space: 51.8 MB (5.08%) - LeetHub

---
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {number}
+ */
+var findKthLargest = function(nums, k) {
+    //edge case
+  if (nums.length === 0){
+    return 0; 
+  }
+
+  let sortedNums = nums.sort((a, b)=> b - a);
+  console.log(sortedNums);
+  let answer = sortedNums.splice(k - 1, 1);
+  return answer;
+};
