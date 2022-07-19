0caf5f14094641c43ab763b955f21e5da36a951a
JRusinova <49450930+jenrusinova@users.noreply.github.com>
2022-07-18 18:08:41 -0700

    Time: 145 ms (61.65%), Space: 55.8 MB (26.21%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.js
+++ 238-product-of-array-except-self/238-product-of-array-except-self.js
@@ -13,16 +13,15 @@ var productExceptSelf = function(nums) {
   let product = 1;
   
   for (let i = 1; i < nums.length; i++){
-    product = nums[i - 1] * answers[i - 1];
+    product = nums[i - 1] * product;
     answers[i] = product;
   }
   product = 1;
   
   console.log(answers); //1,1,2,6
-  for (let i = nums.length - 1; i >=0; i--){
+  for (let i = nums.length - 2; i >=0; i--){
+    product = product* nums[i+1];
     answers[i] = answers[i]*product;
-        product = product*nums[i];
-
 
   }
     return answers;
