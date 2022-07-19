f744342fb72f406cd04fb3b36667b9703275789a
JRusinova <49450930+jenrusinova@users.noreply.github.com>
2022-07-18 18:07:38 -0700

    Time: 177 ms (33.49%), Space: 57 MB (8.81%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.js
+++ 238-product-of-array-except-self/238-product-of-array-except-self.js
@@ -3,29 +3,29 @@
  * @return {number[]}
  */
 var productExceptSelf = function(nums) {
-  let length = nums.length;
-  
-  let left = [];
-  left[0] = 1;
-  let right = [];
-  
-  let answer = [];
-  
-  for (let i =1; i < length; i++){
-    left.push(nums[i - 1] * left[i - 1]);
-  }
-  console.log(left);
-  
-  right[length - 1] = 1;
-  for (let i = length - 2; i >=0; i--){
-    right [i] = right [i+ 1] * nums[i + 1];
-  }
- console.log(right);
-  
-  for (let i = 0; i < left.length; i++){
-    answer[i] = left[i] * right[i];
+    //edge case
+  if (nums.length === 0){
+    return [];
   }
   
-  return answer;
+  let answers = [];
+  answers[0] = 1;
+  let product = 1;
+  
+  for (let i = 1; i < nums.length; i++){
+    product = nums[i - 1] * answers[i - 1];
+    answers[i] = product;
+  }
+  product = 1;
+  
+  console.log(answers); //1,1,2,6
+  for (let i = nums.length - 1; i >=0; i--){
+    answers[i] = answers[i]*product;
+        product = product*nums[i];
+
+
+  }
+    return answers;
+  
   
 };
