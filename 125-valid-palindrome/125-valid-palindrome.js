19aa47769e83e64e2ccc58740bf800d8324afb15
JRusinova <49450930+jenrusinova@users.noreply.github.com>
2022-07-19 19:40:44 -0700

    Time: 158 ms (10.43%), Space: 50.3 MB (13.45%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.js
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -3,29 +3,28 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-    let string = s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
- 
+    //conver string chars to lowercases
   
-  if(string.length === 1){
-    return true;
-  } else if (string.length === 2){
+     let string = s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
   
-    return string[0] === string[1];
-  }
+  if (string.length === 0) return true;
   
-
   let stack = [];
   
-    //iterate on first half of s.length - 1
-  for (let i =0; i < string.length/2; i++){
+  for (let i =0; i <= Math.floor(string.length/2) - 1; i++){
     stack.push(string[i]);
   }
-    
-  for(let i = Math.floor(string.length/2); i < string.length; i++){
-    if (stack[stack.length - 1] === string[i]){
-      stack.pop();
-    }
-  }
 
-   return !stack.length
+   console.log('stack', stack);
+  
+  for (let i = Math.floor(string.length/2); i<string.length; i++){
+   
+
+  if (stack[stack.length - 1] === string[i] ){
+    
+    stack.pop();
+  }
+  }
+  
+   return !stack.length;
 };
