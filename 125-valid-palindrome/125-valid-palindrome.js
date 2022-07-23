870b96e366571a7be1f60f24f202e6f120e640f1
JRusinova <49450930+jenrusinova@users.noreply.github.com>
2022-07-23 16:43:25 -0700

    Time: 364 ms (5.02%), Space: 51.2 MB (9.64%) - LeetHub

--- 125-valid-palindrome/125-valid-palindrome.js
+++ 125-valid-palindrome/125-valid-palindrome.js
@@ -3,28 +3,39 @@
  * @return {boolean}
  */
 var isPalindrome = function(s) {
-    //conver string chars to lowercases
+  //conver string chars to lowercases
   
-     let string = s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
+  let string = s.replace(/[^a-zA-Z0-9]/g, "").toLowerCase();
   
   if (string.length === 0) return true;
+  console.log(string);
+  let left = 0;
+  let right = string.length - 1;
+while (left < right){
   
-  let stack = [];
-  
-  for (let i =0; i <= Math.floor(string.length/2) - 1; i++){
-    stack.push(string[i]);
-  }
+   console.log('left', string[left]);
+    console.log('right', string[right]);
 
-   console.log('stack', stack);
-  
-  for (let i = Math.floor(string.length/2); i<string.length; i++){
-   
-
-  if (stack[stack.length - 1] === string[i] ){
     
-    stack.pop();
+    if(string[left] !== string[right]){
+      return false;
+    }
+    left++;
+    right--;
   }
-  }
+  return true;
   
-   return !stack.length;
+  
+//   let stack = [];
+  
+//   for (let i =0; i <= Math.floor(string.length/2) - 1; i++){
+//     stack.push(string[i]);
+//   }
+  
+//   for (let i = Math.floor(string.length/2); i<string.length; i++){
+//     if (stack[stack.length - 1] === string[i] ){
+//       stack.pop();
+//     }
+//   } 
+//    return !stack.length;
 };
