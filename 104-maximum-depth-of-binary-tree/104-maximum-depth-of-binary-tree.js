656028cca21dd58d00e9faed1ead4c8de20c097e
JRusinova <49450930+jenrusinova@users.noreply.github.com>
2022-07-24 13:41:26 -0700

    Time: 82 ms (78.53%), Space: 44.8 MB (86.66%) - LeetHub

--- 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.js
+++ 104-maximum-depth-of-binary-tree/104-maximum-depth-of-binary-tree.js
@@ -11,36 +11,14 @@
  * @return {number}
  */
 var maxDepth = function(root) {
-
-  if (root === null) {
-    return 0;
-  }
-  let answer = 0;
-  
-  let stack = [];
-  stack.push(root);
-  
-  let depth = [];
-  depth.push(1);
-  
-  while (stack.length){
-    let current = stack.pop();
-    let currentDepth = depth.pop();
     
-    if(current !== null){
-      answer = Math.max(currentDepth, answer);
-      stack.push(current.left);
-      depth.push(currentDepth + 1);
-      stack.push(current.right);
-      depth.push(currentDepth + 1); 
-        
+    if (root === null){
+        return 0;
     }
-  
-  }
-
-return answer;
-  
-
-  
- 
+    
+    var left = maxDepth(root.left);
+    var right = maxDepth(root.right);
+    
+    return Math.max(right, left) + 1;
+    
 };
