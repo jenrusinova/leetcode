24f534056cc263cb1ba667f83e1e16ca3032c6fc
JRusinova <49450930+jenrusinova@users.noreply.github.com>
2022-07-24 14:15:15 -0700

    Time: 76 ms (67.75%), Space: 42.5 MB (25.18%) - LeetHub

--- 100-same-tree/100-same-tree.js
+++ 100-same-tree/100-same-tree.js
@@ -12,18 +12,31 @@
  * @return {boolean}
  */
 var isSameTree = function(p, q) {
+  let isSame = true;
+    if (!p && !q) return true;
+  
+  let firstStack = [p];
+  let secondStack = [q];
+  
+  while(firstStack.length && secondStack.length){
+    let firstCurr = firstStack.pop();
+    let secondCurr = secondStack.pop();
     
-    if (p === null && q === null){
-        return true;
-    }
-    else  if (p === null || q === null){
-        return false;
-    }
-    
-    else if(p.val != q.val){
-        return false;
-    } else{
-   
-    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
-    }
-    };
+    if(firstCurr && secondCurr){
+      if (firstCurr.val!== secondCurr.val) isSame = false;
+        
+   firstStack.push(firstCurr.left);
+  firstStack.push(firstCurr.right);
+ secondStack.push(secondCurr.left);
+  secondStack.push(secondCurr.right);
+      
+    } else if (!firstCurr && secondCurr) isSame = false;
+      else if (firstCurr && !secondCurr) isSame = false;    
+  
+
+
+  
+  }
+  return isSame;
+  
+};
