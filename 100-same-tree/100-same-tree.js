ea8259d21669a033f372974d8d7091d188b042ba
JRusinova <49450930+jenrusinova@users.noreply.github.com>
2022-07-24 14:04:10 -0700

    Time: 72 ms (75.03%), Space: 42.6 MB (25.18%) - LeetHub

--- 100-same-tree/100-same-tree.js
+++ 100-same-tree/100-same-tree.js
@@ -12,37 +12,18 @@
  * @return {boolean}
  */
 var isSameTree = function(p, q) {
-    let isSame = true;
-  
+    
     if (p === null && q === null){
-      return true;
+        return true;
     }
-  
-   let firstStack = [];
-   firstStack.push(p);
-  
-   let secondStack = [];
-   secondStack.push(q);
-  
-   while (firstStack.length && secondStack.length){
-     let firstCurr = firstStack.pop();
-     let secondCurr = secondStack.pop();
-     
-     if(firstCurr !== null && secondCurr !== null) {
-        if (firstCurr.val !== secondCurr.val) {
-          isSame = false;
-        }
-       firstStack.push(firstCurr.left);
-       firstStack.push(firstCurr.right);
-       
-       secondStack.push(secondCurr.left);
-       secondStack.push(secondCurr.right);
-     } else if (firstCurr === null && secondCurr !== null){
-       isSame = false;
-      }else if (firstCurr !== null && secondCurr === null){
-        isSame = false;
-      }
-   } 
-   return isSame;
-  
-};
+    else  if (p === null || q === null){
+        return false;
+    }
+    
+    else if(p.val != q.val){
+        return false;
+    } else{
+   
+    return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+    };
