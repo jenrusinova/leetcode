ed5f63cf062b069de3413c2bcff07d11218063d5
JRusinova <49450930+jenrusinova@users.noreply.github.com>
2022-07-24 20:42:53 -0700

    Time: 86 ms (84.24%), Space: 53.7 MB (78.04%) - LeetHub

---
+++ 334-increasing-triplet-subsequence/334-increasing-triplet-subsequence.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var increasingTriplet = function(nums) {
+  
+ let first = Infinity;
+  let second = Infinity;
+  
+  for(let i =0; i < nums.length; i++){
+    if (nums[i] <= first){
+      first = nums[i];
+    } else if (nums[i] <= second){
+      second = nums[i];
+    } else 
+      return true;
+  }
+  return false;
+  
+};
