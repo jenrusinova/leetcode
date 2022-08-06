68fe8ff1d13b4944694931e2770c7023222902e2
JRusinova <49450930+jenrusinova@users.noreply.github.com>
2022-08-05 20:05:23 -0700

    Time: 184 ms, Memory: 51.9 MB - LeetHub

---
+++ best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(prices) {
+    //edge case
+  if (prices.length === 0){
+    return 0;
+  }
+  
+  let maxProfit = 0;
+  let minPrice = prices[0];
+  
+  for(let i =0; i < prices.length; i++){
+    if(prices[i] < minPrice){
+      minPrice = prices[i];
+    } else {
+      maxProfit = Math.max(maxProfit, prices[i] - minPrice);
+    }
+  }
+  
+  return maxProfit;
+};
+//Time complexity O(n)
