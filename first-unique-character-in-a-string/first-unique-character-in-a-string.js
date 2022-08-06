e6e6368da3d1c0e6e30c179b236b9d6ab0520dcb
JRusinova <49450930+jenrusinova@users.noreply.github.com>
2022-08-05 18:46:24 -0700

    Time: 537 ms, Memory: 44.6 MB - LeetHub

--- first-unique-character-in-a-string/first-unique-character-in-a-string.js
+++ first-unique-character-in-a-string/first-unique-character-in-a-string.js
@@ -14,23 +14,18 @@ var firstUniqChar = function(s) {
     let char = s[i];
     if (obj[char] === undefined && chars.indexOf(char) === -1){
       obj[char] = i;
-          chars.push(char);
+      chars.push(char);
 
     } else {
-      console.log('found duplicate');
       delete obj[char];
     }
   }
-  console.log('char arr', chars);
-console.log('obj', obj);
-  
-  
+ 
   
   let entries = Object.entries(obj);
   if (entries.length === 0) return -1;
   entries.sort((a, b)=> a[1] - b[1]);
-  console.log('entries', entries);
-  console.log(entries[0][1]);
+
   return entries[0][1];
   
   
