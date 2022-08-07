e6a38e917212bdfe45039e7cde827f9ce63cf7a2
JRusinova <49450930+jenrusinova@users.noreply.github.com>
2022-08-07 14:02:34 -0700

    Time: 367 ms (24.76%), Space: 55.2 MB (23.00%) - LeetHub

--- 15-3sum/15-3sum.js
+++ 15-3sum/15-3sum.js
@@ -3,40 +3,40 @@
  * @return {number[][]}
  */
 var threeSum = function(nums) {
-    
-  nums = nums.sort((a, b) => a - b);
+  //O n log n
+  nums.sort((a, b) => a - b);
+  
   let result = [];
   
   
   for (let i =0; i < nums.length - 2; i++){
-    
-       if(i > 0 && nums[i] === nums[i-1]){
-            continue;
-    }
     let left = i + 1;
     let right = nums.length - 1;
     
+    //get rid of duplicates
+    if (i > 0 && nums[i] === nums[i -1]){
+      continue;
+    }
+    
     while (left < right){
-      
-   
-      
       let sum = nums[i] + nums[left] + nums[right];
+      
       if (sum === 0){
-        result.push( [nums[i], nums[left], nums[right]]);
+        result.push([nums[i], nums[left], nums[right]]);
         
-        while(left < right && nums[left] === nums[left+1]) left++;
-        while(left < right && nums[right] === nums[right-1]) right--;
-        
+        //get rid of left and right duplicates
+        while (left < right && nums[left] === nums[left + 1]) left++;
+        while (left < right && nums[right] === nums[right - 1])right--;
         left++;
         right--;
         
-      }else if (sum > 0){
-        right --;
-      }else {
+      } else if (sum < 0){
         left++;
+      } else {
+        right--;
       }
     }
-    
   }
-  return result; 
+  console.log(result);
+    return result;
 };
