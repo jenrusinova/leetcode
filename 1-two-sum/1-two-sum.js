d336a47af7a59954972e2a6575b9f95df32094af
JRusinova <49450930+jenrusinova@users.noreply.github.com>
2022-08-07 13:18:19 -0700

    Time: 1889 ms (5.01%), Space: 66.6 MB (5.11%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -6,14 +6,17 @@
 var twoSum = function(nums, target) {
     let obj = {};
   
-  for(let i =0; i < nums.length; i++){
+  for (let i =0; i < nums.length; i++){
+    console.log(i);
     let diff = target - nums[i];
+    
     for (let key in obj){
       if (obj[key] === diff){
         return [i, key];
       }
     }
-    obj[i] = nums[i];
+    
+        obj[i] = nums[i];
+
   }
-  return [];
 };
